f60e86e301d868cc941debb11f54cc4816f6027d
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-08-28 10:04:05 +0300

    feat: add menu burger and nav interactivity
    Implement interactMenuBurger function. initNavigation on desktop makes nav sticky, on mobile changes layout and always fixed nav

--- ostx_files/header-footer.js
+++ ostx_files/header-footer.js
@@ -1,9 +1,9 @@
-const weatherApi = {
-	key: '9f23b56e8dcad8299bf4e5a2a3fc932b',
-	baseUrl: 'https://api.openweathermap.org/data/2.5/weather'
-}
-
 async function initWeatherWidgets() {
+	const weatherApi = {
+		key: '9f23b56e8dcad8299bf4e5a2a3fc932b',
+		baseUrl: 'https://api.openweathermap.org/data/2.5/weather'
+	}
+
 	const responseJson = await fetch(`${weatherApi.baseUrl}?q=${'London'}&appid=${weatherApi.key}&units=metric`);
 	const weatherObj = await responseJson.json();
 
@@ -20,5 +20,71 @@ async function initWeatherWidgets() {
 		);
 	};
 };
-
 initWeatherWidgets();
+
+function interactMenuBurger() {
+	const menuIcon = document.querySelector('.menu-burger');
+	const menuBody = document.querySelector('.drop-down-menu');
+
+	menuIcon.addEventListener('click', () => {
+		menuIcon.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	})
+}
+interactMenuBurger();
+
+const body = document.querySelector('body');
+
+function initNavigation() {
+	if (window.innerWidth > 890) {
+		placeElementsAsDesctop();
+		const navBlock = document.querySelector('.nav');
+		const headerHeight = document.querySelector('.header__body').offsetHeight;
+
+		navBlock.classList.remove('fixed');
+
+		body.onscroll = () => {
+			if (headerHeight <= scrollY) {
+				navBlock.classList.add('fixed');
+			} else {
+				navBlock.classList.remove('fixed');
+			}
+		};
+	} else {
+		body.onscroll = () => { };
+		const navBlock = document.querySelector('.nav');
+		navBlock.classList.add('fixed');
+		placeElementsAsMobile();
+	}
+
+	function placeElementsAsDesctop() {
+		const titleBlock = document.querySelector('.header__logo');
+		const searchBlock = document.querySelector('.search');
+		const navListBlock = document.querySelector('.nav__list.nav-list');
+
+		const weatherWidget = document.querySelector('.header__weather');
+		const headerRightSection = document.querySelector('.header__right-section');
+		const navContainer = document.querySelector('.nav');
+
+		weatherWidget.after(titleBlock);
+		headerRightSection.prepend(searchBlock);
+		navContainer.prepend(navListBlock);
+	}
+
+	function placeElementsAsMobile() {
+		const titleBlock = document.querySelector('.header__logo');
+		const searchBlock = document.querySelector('.search');
+		const navListBlock = document.querySelector('.nav__list.nav-list');
+
+		const dropDownMenuBlock = document.querySelector('.drop-down-menu');
+		const navContainer = document.querySelector('.nav');
+		const navRightSection = document.querySelector('.nav__right-section');
+
+		navRightSection.prepend(searchBlock);
+		navContainer.prepend(titleBlock);
+		dropDownMenuBlock.prepend(navListBlock);
+	}
+}
+//Breaking point 890px
+initNavigation();
+window.addEventListener('resize', initNavigation);
